645107418902b946f9b1aee8ef357656019743f9
ELIIRRU <ELIIRRU@DESKTOP-ELIIRRU>
2022-10-30 21:31:08 +0100

    Add project files.

---
+++ fishportfolio/Data/MyPokemon.cs
@@ -0,0 +1,16 @@
+﻿using PokeApiNet;
+
+namespace fishportfolio.Data
+{
+    public class MyPokemon
+    {
+        public MyPokemon() { }
+        public MyPokemon(MyPokemon myPokemon)
+        {
+            Pokemon = myPokemon.Pokemon;
+            SpeciesCall = myPokemon.SpeciesCall;
+        }
+        public PokemonSpecies? SpeciesCall { get; set; }
+        public Pokemon Pokemon { get; set; }
+    }
+}
---
+++ fishportfolio/Data/MyTheme.cs
@@ -0,0 +1,28 @@
+﻿using MudBlazor;
+
+namespace fishportfolio.Data
+{
+    public class MyTheme :MudTheme
+    {
+        public MyTheme() :base()
+        {
+            Palette.Primary = "#f4faffff";
+            Palette.Secondary = "#061713";
+            Palette.Tertiary = "#4f646f";
+            Palette.Background = "#3aba9d";
+            Palette.AppbarBackground = "#061713";
+            Palette.DrawerText = "#e6ad87";
+            Palette.DrawerIcon = "#f4faffff";
+            Palette.DrawerBackground = "#061713";
+            Palette.Divider = "#f4faffff";
+            Palette.TextPrimary = "#535657";
+            Palette.TextSecondary = "#42f5f2";
+            Palette.TextDisabled = "#42f5f2";
+            Palette.ActionDisabled = "#8e9499";
+            Typography.H1.FontSize = "2rem";
+            Typography.H4.FontSize = "1.2rem";
+            Typography.H5.FontSize = "1rem";
+            Typography.H6.FontSize = "0.8rem";
+        }
+    }
+}
